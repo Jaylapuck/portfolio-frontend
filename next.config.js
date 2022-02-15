0dbe3b4ba11512c69dd8c84959f7184e42426859
Jeremy Forest <53579676+Jaylapuck@users.noreply.github.com>
2022-02-15 11:05:47 -0500

    added new image provider.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com','localhost:3000','localhost:1337',"localhost", "strapi-portfolio-deploy.herokuapp.com"],
+    domains: ['avatars.githubusercontent.com','localhost:3000','localhost:1337',"res.cloudinary.com", "strapi-portfolio-deploy.herokuapp.com"],
   },
 }
